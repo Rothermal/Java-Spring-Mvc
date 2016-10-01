93cf31ac9829ced440f2a018a2a8ee169133a2a8
Mark Rothermal <rothermal2@comcast.net>
2016-10-01 15:46:28 -0500

    setting up jquery

---
+++ web/index.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Mark on 10/1/16.
+ */
+$(document).ready(function(){
+    init();
+    enable();
+});
+
+function init (){
+    console.log('jquery running init');
+}
+function enable (){
+    console.log('jquery enabled');
+}
